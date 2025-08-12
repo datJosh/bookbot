076a132560f1255796d48f0d5c0cde339e6a4acc
datJosh <josh.kirrb@gmail.com>
2025-08-12 19:43:24 +0200

    count words

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(str):
+    words = str.split()
+    num_words = len(words)
+    print(f"{num_words} words found in the document")
+
 def main():
     book_str = get_book_text('books/frankenstein.txt')
-    print(book_str)
+    count_words(book_str)
 
 main()
